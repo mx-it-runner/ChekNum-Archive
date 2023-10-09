01045d158a6e573bdbe85e37436f89c3aa6d64eb
Maxim <72748980+mx-it-runner@users.noreply.github.com>
2023-10-09 13:39:22 +0500

    fix sort len(x)
    Подключили сортировка по длине номера

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
 import pandas as pd
-import numpy as np
+import sys
+#import numpy as np
 
 # Чтение данных из файлов DEF-9xx.xlsx и numbers.xlsx
 def_data = pd.read_excel('Data.xlsx')
@@ -14,22 +14,25 @@ mapped_data = []
 for number in numbers_data['Numbers']:
     # Получение кода оператора (АБС) и номера для диапазона
     x = str(number)
-    kod_operatora = x[1:4]
-    nomer =  x[4:11]
+    if (len(x)) == 11:
 
-    # Поиск соответствующего диапазона значений "От" и "До"
+        kod_operatora = x[1:4]
+        nomer =  x[4:11]    
+        # Поиск соответствующего диапазона значений "От" и "До"
     
-    match = def_data[(def_data['АВС/ DEF'] == int(kod_operatora)) & (def_data['От'] <= int(nomer)) & (def_data['До'] >= int(nomer))]
-
+        match = def_data[(def_data['АВС/ DEF'] == int(kod_operatora)) & (def_data['От'] <= int(nomer)) & (def_data['До'] >= int(nomer))]    
     
-    # Проверка, найдено ли совпадение
-    if not match.empty:
+        # Проверка, найдено ли совпадение
+        if not match.empty:
         # Получение значений оператора и региона для найденного диапазона
-        operator = match['Оператор'].iloc[0]
-        region = match['Регион'].iloc[0]
+            operator = match['Оператор'].iloc[0]
+            region = match['Регион'].iloc[0]
         
         # Добавление сопоставленных данных в список
-        mapped_data.append([number, operator, region])
+            mapped_data.append([number, operator, region])
+    else:
+        print("Ошибочка")
+
 
 # Создание DataFrame для сопоставленных данных
 mapped_df = pd.DataFrame(mapped_data, columns=['Номер', 'Оператор сотовой связи', 'Регион'])
